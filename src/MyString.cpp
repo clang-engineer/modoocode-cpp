52a9549b5130aa0f917dd373c655453506ed444f
zero <yorez@naver.com>
2022-05-22 13:00:33 +0000

    modify: assgn func add

--- src/MyString.cpp
+++ src/MyString.cpp
@@ -16,6 +16,8 @@ class MyString{
         int length() const;
         void print() const;
         void println() const;
+        MyString& assign(const MyString& str);
+        MyString& assign(const char* str);
 };
 
 MyString::MyString(char c){
@@ -62,13 +64,47 @@ void MyString::println() const {
     std::cout << std::endl;
 };
 
+MyString& MyString::assign(const MyString& str) {
+    if(str.string_length > string_length){
+        delete[] string_content;
+        string_content = new char[str.string_length];
+    };
+
+    for (int i=0; i<str.string_length; i++){
+        string_content[i] = str.string_content[i];
+    };
+
+    string_length = str.string_length;
+
+    return *this;
+};
+
+MyString& MyString::assign(const char* str) {
+    int str_length = strlen(str);
+    if(str_length > string_length) {
+        delete[] string_content;
+        string_content = new char[str_length];
+    };
+
+    for (int i=0; i< str_length; i++){
+        string_content[i] = str[i];
+    };
+
+    string_length = str_length;
+
+    return *this;
+};
+
 
 int main(){
-    MyString str1("hello world");
-    MyString str2 = str1;
-
+    MyString str1("very very long string");
     str1.println();
-    str2.println();
+
+    str1.assign("short string");
+    str1.println();
+
+    str1.assign("very long string");
+    str1.println();
 };
 
 
