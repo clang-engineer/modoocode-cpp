429e0c0f97f7a7b8e877f9d3c985b5a2714b580f
zero <yorez@naver.com>
2022-05-22 09:11:59 +0000

    modify: return ref

--- src/Marine.cpp
+++ src/Marine.cpp
@@ -3,6 +3,7 @@
 class Marine {
     private:
         static int total_marine_num;
+        const static int i = 0;
 
         int hp;
         int coord_x, coord_y;
@@ -16,10 +17,11 @@ class Marine {
         Marine(int x, int y, int default_damage);
 
         int attack();
-        void be_attacked(int damage_earn);
+        Marine& be_attacked(int damage_earn);
         void move(int x, int y);
 
         void show_statuts();
+        static void show_total_marine();
         ~Marine() {
             total_marine_num-=1;
             std::cout << "~Marine() -> marine num : " << total_marine_num <<std::endl;
@@ -27,6 +29,10 @@ class Marine {
 };
 
 int Marine::total_marine_num = 0;
+
+void Marine::show_total_marine(){
+    std::cout << "total marin num: " << total_marine_num << std::endl;
+};
 
 Marine::Marine() : hp(50), coord_x(0), coord_y(0), default_damage(5), is_dead(false) {
     total_marine_num +=1;
@@ -43,15 +49,17 @@ Marine::Marine(int x, int y, int default_damage) : coord_x(x),coord_y(y), hp(50)
 void Marine::move(int x, int y) {
     coord_x = x;
     coord_y = y;
+    total_marine_num +=1;
 };
 
 int Marine::attack(){
     return default_damage;
 };
 
-void Marine::be_attacked(int damage_earn) {
+Marine& Marine::be_attacked(int damage_earn) {
     hp-=damage_earn;
     if(hp<=0) is_dead = true; 
+    return *this;
 };
 
 void Marine::show_statuts(){
@@ -72,10 +80,8 @@ int main(){
     Marine marine2(3,5,10);
     marine2.show_statuts();
 
-    create_marine();
-
     std::cout << std::endl << "marine1 attack marine2" << std::endl;
-    marine2.be_attacked(marine1.attack());
+    marine2.be_attacked(marine1.attack()).be_attacked(marine1.attack());
 
     marine1.show_statuts();
     marine2.show_statuts();
