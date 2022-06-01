b61f87f936b52510ed0f331f99b2865b607b05cc
zero <yorez@naver.com>
2022-06-01 13:35:57 +0000

    feat: Vector - add sorting func template

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -42,6 +42,12 @@ class Vector {
             return length;
         }
 
+        void swap(int i, int j) {
+            T temp = data[i];
+            data[i] = data[j];
+            data[j] = temp;
+        }
+
         ~Vector() {
             if (data) {
                 delete[] data;
@@ -49,120 +55,37 @@ class Vector {
         }
 };
 
-template <>
-class Vector<bool> {
-
-    private:
-        unsigned int* data;
-        int capacity;
-        int length;
-
-    public:
-        typedef bool value_type;
-
-        Vector(int n = 1): data(new unsigned int[n/32 + 1]), capacity(n/32 + 1), length(0) {
-            for (int i = 0; i < capacity; i++) {
-                data[i] = 0;
+template <typename Cont>
+void bubble_sort(Cont& cont) {
+    for (int i = 0; i < cont.size(); i++) {
+        for (int j = i + 1; j < cont.size(); j++) {
+            if (cont[i] > cont[j]) {
+                cont.swap(i, j);
             }
         }
-
-        void push_back(bool s) {
-            if (capacity * 32 <= length) {
-                unsigned int* temp = new unsigned int[capacity * 2];
-                for (int i = 0; i < capacity; i++) {
-                    temp[i] = data[i];
-                }
-
-                for (int i = capacity; i < 2 * capacity; i++) {
-                    temp[i] = 0;
-                }
-
-                delete[] data;
-                data = temp;
-                capacity *= 2;
-            }
-
-            if (s) {
-                data[length / 32] |= (1 << (length % 32));
-            }
-
-            length++;
-        }
-
-        bool operator[](int i) {
-            return (data[i / 32] & (1 << (i % 32))) != 0;
-        }
-
-        void remove(int x) {
-            for (int i = x + 1; i < length; i++) {
-                int prev = i - 1;
-                int curr = i;
-
-                if (data[curr / 32] & (1 << (curr % 32))) {
-                    data[prev / 32] |= (1 << (prev % 32));
-                } else {
-                    unsigned int all_ones_except_prev = 0xFFFFFFFF;
-                    all_ones_except_prev ^= (1 << (prev % 32));
-                    data[prev / 32] &= all_ones_except_prev;
-                }
-            }
-            length--;
-        }
-
-        int size() {return length;}
-
-        ~Vector() {
-            if(data) {
-                delete[] data;
-            }
-        }
-};
+    }
+}
 
 
 int main() {
-
     Vector<int> int_vec;
+
     int_vec.push_back(3);
-    int_vec.push_back(4);
+    int_vec.push_back(1);
+    int_vec.push_back(2);
+    int_vec.push_back(8);
+    int_vec.push_back(5);
+    int_vec.push_back(3);
 
-    std::cout << "== int vector ==" << std::endl;
-    std::cout << int_vec[0] << std::endl;
-    std::cout << int_vec[1] << std::endl;
+    std::cout << "==before sort==" << std::endl;
+    for (int i = 0; i < int_vec.size(); i++) {
+        std::cout << int_vec[i] << " ";
+    }
 
-    Vector<std::string> str_vec;
-    str_vec.push_back("hello");
-    str_vec.push_back("world");
-
-    std::cout << "== string vector ==" << std::endl;
-    std::cout << str_vec[0] << std::endl;
-    std::cout << str_vec[1] << std::endl;
-
-    Vector<bool> bool_vec;
-    bool_vec.push_back(true);
-    bool_vec.push_back(true);
-    bool_vec.push_back(false);
-    bool_vec.push_back(false);
-    bool_vec.push_back(false);
-    bool_vec.push_back(true);
-    bool_vec.push_back(false);
-    bool_vec.push_back(true);
-    bool_vec.push_back(false);
-    bool_vec.push_back(true);
-    bool_vec.push_back(false);
-    bool_vec.push_back(true);
-    bool_vec.push_back(false);
-    bool_vec.push_back(true);
-    bool_vec.push_back(false);
-    bool_vec.push_back(true);
-    bool_vec.push_back(false);
-    bool_vec.push_back(true);
-    bool_vec.push_back(false);
-
-    std::cout << "== bool vector ==" << std::endl;
-    for (int i = 0; i < bool_vec.size(); i++) {
-        std::cout << bool_vec[i];
+    std::cout << std::endl << "==after sort==" << std::endl;
+    bubble_sort(int_vec);
+    for (int i = 0; i < int_vec.size(); i++) {
+        std::cout << int_vec[i] << " ";
     }
     std::cout << std::endl;
-
-    return 0;
 }
