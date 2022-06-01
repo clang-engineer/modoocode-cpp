bdf6ecef5460cb783dac9274c2f9a3c2a1b91b0f
zero <yorez@naver.com>
2022-06-01 06:30:24 +0000

    feat: FStream - read all file content

--- src/FStream.cpp
+++ src/FStream.cpp
@@ -7,20 +7,18 @@ int main() {
     std::string s;
 
     if (in.is_open()) {
-        in >> s;
+        in.seekg(0, std::ios::end);
+        int size = in.tellg();
+
+        s.resize(size);
+
+        in.seekg(0, std::ios::beg);
+
+        in.read(&s[0], size);
         std::cout << "input string is: " << s << std::endl;
     } else {
         std::cout << "can not find file" << s << std::endl;
     }
 
-    in.close();
-    in.open("other.txt");
-    
-    if (in.is_open()) {
-        in >> s;
-        std::cout << "input string is: " << s << std::endl;
-    } else {
-        std::cout << "can not find file" << s << std::endl;
-    }
     return 0;
 }
