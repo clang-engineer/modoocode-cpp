4fe0207808461df284174653aa2999063205f627
zero <yorez@naver.com>
2022-06-06 12:43:33 +0000

    feat: Lamda Transform ex

---
+++ src/ch11-2/Transform.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename Iter>
+void print(Iter begin, Iter end) {
+    while (begin != end) {
+        std::cout << "["<< *begin << "]"; 
+        begin++;
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> vec;
+
+    vec.push_back(5);
+    vec.push_back(3);
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+
+
+    std::cout << "before transform" << std::endl;
+    print(vec.begin(), vec.end());
+
+    std::cout << "after transform" << std::endl;
+    std::transform(vec.begin(), vec.end(), vec.begin(), 
+            [](int i) -> int {return i*2;});
+    print(vec.begin(), vec.end());
+}
