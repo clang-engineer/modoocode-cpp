83ab60afc50521c572d23bebdf25a13521a1e046
zero <yorez@naver.com>
2022-06-06 07:33:44 +0000

    feat: Sort.cpp - default layout

---
+++ src/ch11-2/Sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+void print_vec(std::vector<T>& vec) {
+    for (const auto& elem : vec) {
+        std::cout << elem << std::endl;
+    }
+
+}
+int main() {
+    std::vector<int> vec;
+
+    vec.push_back(5);
+    vec.push_back(3);
+    vec.push_back(1);
+    vec.push_back(6);
+    vec.push_back(4);
+    vec.push_back(7);
+    vec.push_back(2);
+    
+    std::cout << "before sort" << std::endl;
+    print_vec(vec);
+
+    std::cout << "after sort" << std::endl;
+    std::sort(vec.begin(), vec.end());
+    print_vec(vec);
+
+}
