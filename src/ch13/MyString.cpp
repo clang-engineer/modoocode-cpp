9900d3c1b45ee0aa336b3340acab8667b980f388
zero <yorez@naver.com>
2022-06-19 02:56:14 +0000

    chore

--- src/ch13/MyString.cpp
+++ src/ch13/MyString.cpp
@@ -17,8 +17,13 @@ class MyString {
 
         MyString(MyString &&str) noexcept;
 
+        MyString &operator=(const MyString& s);
+
         ~MyString();
 
+        int length() const;
+
+        void println();
 };
 
 MyString::MyString() {
@@ -60,23 +65,57 @@ MyString::MyString(MyString &&str) noexcept {
 }
 
 
+MyString &MyString::operator=(const MyString& s) {
+    std::cout << "Call operator=" << std::endl;
+    if (s.string_length > memory_capacity) {
+        delete[] string_content;
+        string_content = new char[s.string_length];
+        memory_capacity = s.string_length;
+    }
+
+    string_length = s.string_length;
+    for (int i = 0; i < string_length; i++) {
+        string_content[i] = s.string_content[i];
+    }
+
+    return *this;
+};
+
 MyString::~MyString() {
     if (string_content) {
         delete[] string_content;
     }
 }
 
+int MyString::length() const {
+    return string_length;
+}
+
+void MyString::println() {
+    for (int i = 0; i < string_length; i++) {
+        std::cout << string_content[i];
+    }
+
+    std::cout << std::endl;
+}
+
+template <typename T>
+void my_swap(T &a, T &b) {
+    T tmp(a);
+    a = b;
+    b = tmp;
+}
+
 int main() {
-    MyString s("abc");
-    std::vector<MyString> vec;
-    
-    std::cout << "===========add first elem=========" << std::endl;
-    vec.push_back(s);
+    MyString str1("abc");
+    MyString str2("def");
 
-    std::cout << "===========add second elem=========" << std::endl;
+    std::cout << "Before Swap ========" << std::endl;
+    str1.println();
+    str2.println();
 
-    vec.push_back(s);
-
-    std::cout << "===========add third elem=========" << std::endl;
-    vec.push_back(s);
+    std::cout << "After Swap ========" << std::endl;
+    my_swap(str1, str2);
+    str1.println();
+    str2.println();
 }
